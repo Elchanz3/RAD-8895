fa077cc29fedc2007e85df771ad55f637753c88c
ace2nutzer <ace2nutzer@gmail.com>
2022-06-21 01:47:45 +0200

    cal-if : unlock voltage control

--- drivers/cpufreq/exynos-ufc.c
+++ drivers/cpufreq/exynos-ufc.c
@@ -536,6 +536,63 @@ static ssize_t store_execution_mode_change(struct kobject *kobj, struct attribut
 	return count;
 }
 
+static ssize_t store_cpu_lit_volt(struct kobject *kobj, struct attribute *attr, const char *buf, size_t count)
+{
+	int id = 3; /* dvfs_cpucl1 */
+	unsigned int rate, volt;
+
+	if (sscanf(buf, "%u %u", &rate, &volt) == 2) {
+		if ((volt < 450000) || (volt > 1400000))
+			goto err;
+		update_fvmap(id, rate, volt);
+		pr_info("%s: updated DVFS: dvfs_cpucl1 - rate: %u kHz - volt: %u uV\n", __func__, rate, volt);
+		return count;
+	}
+
+err:
+	return -EINVAL;
+}
+
+static ssize_t store_cpu_big_volt(struct kobject *kobj, struct attribute *attr, const char *buf, size_t count)
+{
+	int id = 2; /* dvfs_cpucl0 */
+	unsigned int rate, volt;
+
+	if (sscanf(buf, "%u %u", &rate, &volt) == 2) {
+		if ((volt < 450000) || (volt > 1400000))
+			goto err;
+		update_fvmap(id, rate, volt);
+		pr_info("%s: updated DVFS: dvfs_cpucl0 - rate: %u kHz - volt: %u uV\n", __func__, rate, volt);
+		return count;
+	}
+
+err:
+	return -EINVAL;
+}
+
+static ssize_t store_update_dvfs_table(struct kobject *kobj, struct attribute *attr, const char *buf, size_t count)
+{
+	unsigned int id, rate, volt;
+
+	if (sscanf(buf, "%u %u %u", &id, &rate, &volt) == 3) {
+		update_fvmap(id, rate, volt);
+		pr_info("%s: updated DVFS id: %u - rate: %u kHz - volt: %u uV\n", __func__, id, rate, volt);
+		return count;
+	}
+
+	return -EINVAL;
+}
+
+static ssize_t store_print_dvfs_table(struct kobject *kobj, struct attribute *attr, const char *buf, size_t count)
+{
+	if (sysfs_streq(buf, "true") || sysfs_streq(buf, "1")) {
+		print_fvmap();
+		return count;
+	}
+
+	return -EINVAL;
+}
+
 static struct global_attr cpufreq_table =
 __ATTR(cpufreq_table, 0444, show_cpufreq_table, NULL);
 static struct global_attr cpufreq_min_limit =
@@ -550,6 +607,12 @@ __ATTR(cpufreq_max_limit, 0644,
 static struct global_attr execution_mode_change =
 __ATTR(execution_mode_change, 0644,
 		show_execution_mode_change, store_execution_mode_change);
+static struct global_attr sysfs_cpu_lit_volt =
+__ATTR(cpu_lit_volt, 0600,
+		NULL, store_cpu_lit_volt);
+static struct global_attr sysfs_cpu_big_volt =
+__ATTR(cpu_big_volt, 0600,
+		NULL, store_cpu_big_volt);
 
 static __init void init_sysfs(void)
 {
@@ -568,6 +631,11 @@ static __init void init_sysfs(void)
 	if (sysfs_create_file(power_kobj, &execution_mode_change.attr))
 		pr_err("failed to create cpufreq_max_limit node\n");
 
+	if (sysfs_create_file(power_kobj, &sysfs_cpu_lit_volt.attr))
+		pr_err("failed to create cpu_lit_volt node\n");
+
+	if (sysfs_create_file(power_kobj, &sysfs_cpu_big_volt.attr))
+		pr_err("failed to create cpu_big_volt node\n");
 }
 
 static int parse_ufc_ctrl_info(struct exynos_cpufreq_domain *domain,
